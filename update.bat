1afa8d38fd12ded0ca83e01b13209145432c120f
dreamness <dreamness@163.com>
2024-12-23 10:51:22 +0800

    修改更新程序

--- update.bat
+++ update.bat
@@ -1,43 +1,43 @@
 @echo off
-@chcp 65001 >nul
+@chcp 936 >nul
 
-:: 检查是否安装了 Git
+:: ����Ƿ�װ�� Git
 git --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo [错误] 未检测到 Git，请先安装 Git.
+    echo [����] δ��⵽ Git�����Ȱ�װ Git.
     pause
     exit /b
 )
 
-:: 提示用户操作
+:: ��ʾ�û�����
 cls
-echo 将要更新 lua 库到最新版本
+echo ��Ҫ���� lua �⵽���°汾
 echo.
-echo 警告: 对 lua 库的所有更改都会被覆盖
+echo ����: �� lua ������и��Ķ��ᱻ����
 echo.
-echo [y] 确认 / [n] 取消
+echo [y] ȷ�� / [n] ȡ��
 set "input=y"
-set /p input=请输入: [默认为y] 
+set /p input=������: [Ĭ��Ϊy] 
 
-:: 判断用户输入
+:: �ж��û�����
 echo.
 if /i "%input%"=="n" (
-    echo 更新已取消.
+    echo ������ȡ��.
     pause
     exit /b
 )
 
 if not "%input%"=="y" (
-    echo 未识别的输入, 默认继续.
+    echo δʶ�������, Ĭ�ϼ���.
 )
 
-:: 强行拉取远程分支并覆盖本地所有更改
-echo 正在更新 lua 库...
+:: ǿ����ȡԶ�̷�֧�����Ǳ������и���
+echo ���ڸ��� lua ��...
 git clean -fd
 git reset --hard
 git fetch --all
 git reset --hard origin/main
 
-:: 提示完成
-echo 更新完成.
+:: ��ʾ���
+echo �������.
 pause
