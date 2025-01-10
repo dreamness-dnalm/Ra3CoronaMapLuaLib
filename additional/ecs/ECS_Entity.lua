f8159979f5f5187c8e0d6992111d2d5920b0fb43
dreamness <dreamness@163.com>
2025-01-10 09:07:56 +0800

    upload

--- additional/ecs/ECS_Entity.lua
+++ additional/ecs/ECS_Entity.lua
@@ -3,7 +3,8 @@
 --- @field components ECS_Component[]
 ECS_Entity = {}
 
-
+--- @param component_name string
+--- @return boolean
 function ECS_Entity:has_component(component_name)
     for i = 1, getn(self.components) do
         local component = self.components[i]
@@ -14,4 +15,29 @@ function ECS_Entity:has_component(component_name)
     end
 
     return false
+end
+
+--- @param component ECS_Component
+function ECS_Entity:remove_component_by_ref(component)
+    for i = 1, getn(self.components) do
+        if self.components[i] == component then
+            tremove(self.components, i)
+        end
+    end
+end
+
+--- @param component_name string
+function ECS_Entity:remove_component_by_component_name(component_name)
+    for i = 1, getn(self.components) do
+        local component = self.components[i]
+
+        if component.component_name == component_name then
+            tremove(self.components, i)
+        end
+    end
+end
+
+--- @param component ECS_Component
+function ECS_Entity:add_component(component)
+    tinsert(self.components, component)
 end
--- additional/ecs/ECS_Helper.lua
+++ additional/ecs/ECS_Helper.lua
@@ -23,3 +23,38 @@ ECS_Helper.new_ECS_Manager = function()
 end
 
 --- @return ECS_Entity
+ECS_Helper.new_ECS_Entity = function()
+    --- @type ECS_Entity
+    local obj = { components = {} }
+
+    settag(obj, ECS_Helper._meta.tags.ECS_Entity_tag)
+
+    settagmethod(_meta.tags.unit_tag, "index", function(t, key)
+        return Unit[key]
+    end)
+
+    return obj
+end
+
+--- @param required_component_names string[]
+--- @param optional_component_names string[]
+--- @param excluded_component_names string[]
+--- @param update fun(self:ECS_System, required_components: ECS_Component[], optional_components: ECS_Component[], entity: ECS_Entity)
+--- @return ECS_Component
+ECS_Helper.new_ECS_System = function(required_component_names, optional_component_names, excluded_component_names, update)
+    --- @type ECS_System
+    local obj = { 
+        required_component_names = required_component_names,
+        optional_component_names = optional_component_names,
+        excluded_component_names = excluded_component_names,
+        update = update
+    }
+
+    settag(obj, ECS_Helper._meta.tags.ECS_System_tag)
+
+    settagmethod(_meta.tags.unit_tag, "index", function(t, key)
+        return Unit[key]
+    end)
+
+    return obj
+end
