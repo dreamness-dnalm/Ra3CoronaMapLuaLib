079968c49a1b2f606e848d31ef01cacf33d8d7d1
dreamness <dreamness@163.com>
2025-02-20 23:06:04 +0800

    upload

--- lib/enums/ProtocolEnum.lua
+++ lib/enums/ProtocolEnum.lua
@@ -1,71 +1,176 @@
---- SpecialPowerEnum.lua
---- Created by dreamness
---- DateTime: 2024/11/24 22:53
----
 
---- @class SpecialPowerEnum
-SpecialPowerEnum = {
+--- @class ProtocolEnum
+ProtocolEnum = {
+    ----------------- 盟军 -------------------
+    
+    ------ RANK1---------
+    --- 侦查协议
     SpecialPowerSatelliteSweep = "SpecialPowerSatelliteSweep",
+    --- 先进航空学
     SpecialPowerAirPower = "SpecialPowerAirPower",
+    --- 高科技
     SpecialPowerHighTechnology = "SpecialPowerHighTechnology",
+
+    ------ RANK2---------
+    --- 精确打击
     SpecialPowerPrecisionStrike = "SpecialPowerPrecisionStrike",
+    --- 冷冻卫星1
     SpecialPowerCryoSatelliteLvl1 = "SpecialPowerCryoSatelliteLvl1",
+    --- 超时空互换1
     SpecialPowerChronoSwapSelect = "SpecialPowerChronoSwapSelect",
+    --- 超时空互换2
     SpecialPowerChronoSwapTeleport = "SpecialPowerChronoSwapTeleport",
 
+    ------ RANK3---------
+    --- 时空裂缝1
     SpecialPowerChronoRiftSelectLvl1 = "SpecialPowerChronoRiftSelectLvl1",
+    --- 冷冻卫星2
     SpecialPowerCryoSatelliteLvl2 = "SpecialPowerCryoSatelliteLvl2",
+    --- 时空炸弹1
     SpecialPowerTimeBombLvl1 = "SpecialPowerTimeBombLvl1",
+
+    ------ RANK4---------
+    --- 时空裂缝2
     SpecialPowerChronoRiftSelectLvl2 = "SpecialPowerChronoRiftSelectLvl2",
+    --- 冷冻卫星3
     SpecialPowerCryoSatelliteLvl3 = "SpecialPowerCryoSatelliteLvl3",
+    --- 时空炸弹2
     SpecialPowerTimeBombLvl2 = "SpecialPowerTimeBombLvl2",
+
+    ------ RANK5---------
+    --- 时空裂缝3
     SpecialPowerChronoRiftSelectLvl3 = "SpecialPowerChronoRiftSelectLvl3",
+    --- 自由贸易
     SpecialPowerAlliedFreeTrade = "SpecialPowerAlliedFreeTrade",
+    --- 时空炸弹3
     SpecialPowerTimeBombLvl3 = "SpecialPowerTimeBombLvl3",
+
+    --------- 神舟 --------------------------------
+
+    ------ RANK1---------
+    --- 彻甲惊雷（电磁炮）
     SpecialPower_Celestial_ElectromagneticGun = "SpecialPower_Celestial_ElectromagneticGun",
+    --- 百夫长
     SpecialPower_CelestialCenturionUpgrade = "SpecialPower_CelestialCenturionUpgrade",
+    --- 电能纳贡
     SpecialPower_CelestialPowerSealOff = "SpecialPower_CelestialPowerSealOff",
+
+    ------ RANK2---------
+    --- 元神互锁1
     SpecialPower_CelestialQuantumBreak = "SpecialPower_CelestialQuantumBreak",
+    --- 元神互锁2
     SpecialPower_CelestialQuantumLink = "SpecialPower_CelestialQuantumLink",
+    --- 四方玉宇lv1
     SpecialPower_Celestial_EMSuppressField_Lv1 = "SpecialPower_Celestial_EMSuppressField_Lv1",
-    SpecialPower_Celestial_EMSuppressField_Lv2 = "SpecialPower_Celestial_EMSuppressField_Lv2",
+    --- 陨星天火(天火塔)
     SpecialPowerCelestialEMTurretDrop = "SpecialPowerCelestialEMTurretDrop",
+
+    ------ RANK3---------
+    --- 羲和焚风lv1
     SpecialPowerCelestialSatelliteLaserSweep_Lv1 = "SpecialPowerCelestialSatelliteLaserSweep_Lv1",
+    --- 六面琼楼lv2
+    SpecialPower_Celestial_EMSuppressField_Lv2 = "SpecialPower_Celestial_EMSuppressField_Lv2",
+    --- 雷铸天兵：百骑lv1
     SpecialPower_CelestiallightningTroopUpgrade_Lv1 = "SpecialPower_CelestiallightningTroopUpgrade_Lv1",
+
+    ------ RANK4---------
+    --- 羲和阳炎lv2
     SpecialPowerCelestialSatelliteLaserSweep_Lv2 = "SpecialPowerCelestialSatelliteLaserSweep_Lv2",
+    --- 八门金锁lv3
     SpecialPower_Celestial_EMSuppressField_Lv3 = "SpecialPower_Celestial_EMSuppressField_Lv3",
+    --- 雷铸天兵：千军lv2
     SpecialPower_CelestiallightningTroopUpgrade_Lv2 = "SpecialPower_CelestiallightningTroopUpgrade_Lv2",
+
+    ------ RANK5---------
+    --- 羲和十日煌lv3
     SpecialPowerCelestialSatelliteLaserSweep_Lv3 = "SpecialPowerCelestialSatelliteLaserSweep_Lv3",
+    --- 三子归边
     SpecialPower_CelestialSpaceReinforce = "SpecialPower_CelestialSpaceReinforce",
+    --- 雷铸天兵：万乘lv3
     SpecialPower_CelestiallightningTroopUpgrade_Lv3 = "SpecialPower_CelestiallightningTroopUpgrade_Lv3",
+    
+    ----------------- 帝国 -------------------
+    
+    ------ RANK1---------
+    --- 海军协议
     SpecialPowerJapanNavalPower = "SpecialPowerJapanNavalPower",
+    --- 先进导弹协议
     SpecialPowerJapanAdvancedMissilePacks = "SpecialPowerJapanAdvancedMissilePacks",
+    --- 自爆协议
     SpecialPowerJapanEnhancedKamikaze = "SpecialPowerJapanEnhancedKamikaze",
+    
+    ------ RANK2---------
+    --- 帽子协议
     SpecialPowerJapanAmbush = "SpecialPowerJapanAmbush",
+    --- 决胜中队1级
     SpecialPowerFinalSquadron_L1 = "SpecialPowerFinalSquadron_L1",
+    --- 套子协议
     SpecialPower_PointDefenseDrones = "SpecialPower_PointDefenseDrones",
+
+    ------ RANK3---------
+    --- 气球炸弹1级
     SpecialPowerJapanBalloonAttack_L1 = "SpecialPowerJapanBalloonAttack_L1",
+    --- 决胜中队2级
     SpecialPowerFinalSquadron_L2 = "SpecialPowerFinalSquadron_L2",
+    --- 天皇协议1级
     SpecialPowerJapanEmperorsResolve_L1 = "SpecialPowerJapanEmperorsResolve_L1",
+
+    ------ RANK4---------
+    --- 气球炸弹2级
     SpecialPowerJapanBalloonAttack_L2 = "SpecialPowerJapanBalloonAttack_L2",
+    --- 决胜中队3级
     SpecialPowerFinalSquadron_L3 = "SpecialPowerFinalSquadron_L3",
+    --- 天皇协议2级
     SpecialPowerJapanEmperorsResolve_L2 = "SpecialPowerJapanEmperorsResolve_L2",
+
+    ------ RANK5---------
+    --- 气球炸弹3级
     SpecialPowerJapanBalloonAttack_L3 = "SpecialPowerJapanBalloonAttack_L3",
+    --- 机械化组装
     SpecialPowerJapanRoboticAssembly = "SpecialPowerJapanRoboticAssembly",
+    --- 天皇协议3级
     SpecialPowerJapanEmperorsResolve_L3 = "SpecialPowerJapanEmperorsResolve_L3",
+
+    ----------------- 苏联 -------------------
+
+    ------ RANK1---------
+    --- 碾压协议
     SpecialPowerCrushPuppies = "SpecialPowerCrushPuppies",
+    --- 蜘蛛协议
     SpecialPowerTerrorDroneEggs = "SpecialPowerTerrorDroneEggs",
+    --- 现金协议
     SpecialPower_ProductionKickbacks = "SpecialPower_ProductionKickbacks",
+
+    ------ RANK2---------
+    --- 毒质腐蚀
     SpecialPower_IrradiateTarget = "SpecialPower_IrradiateTarget",
+    --- 轨道垃圾1级
     SpecialPowerOrbitalRefuse_Rank1 = "SpecialPowerOrbitalRefuse_Rank1",
+    --- 撒毒协议1级
     SpecialPower_DesolatorBomb_Level1 = "SpecialPower_DesolatorBomb_Level1",
+
+    ------ RANK3---------
+    --- 磁力卫星1级
     SpecialPowerMagneticSatelliteLvl1 = "SpecialPowerMagneticSatelliteLvl1",
+    --- 轨道垃圾2级
     SpecialPowerOrbitalRefuse_Rank2 = "SpecialPowerOrbitalRefuse_Rank2",
+    --- 撒毒协议2级
     SpecialPower_DesolatorBomb_Level2 = "SpecialPower_DesolatorBomb_Level2",
+
+    ------ RANK4---------
+    --- 磁力卫星2级
     SpecialPowerMagneticSatelliteLvl2 = "SpecialPowerMagneticSatelliteLvl2",
+    --- 轨道垃圾3级
     SpecialPowerOrbitalRefuse_Rank3 = "SpecialPowerOrbitalRefuse_Rank3",
+    --- 撒毒协议3级
     SpecialPower_DesolatorBomb_Level3 = "SpecialPower_DesolatorBomb_Level3",
+
+    ------ RANK5---------
+    --- 磁力卫星3级
     SpecialPowerMagneticSatelliteLvl3 = "SpecialPowerMagneticSatelliteLvl3",
+    --- 大生产
     SpecialPowerSovietMassProduction = "SpecialPowerSovietMassProduction",
+    --- 磁力奇点
     SpecialPowerMagneticSingularity = "SpecialPowerMagneticSingularity"
+
 }
