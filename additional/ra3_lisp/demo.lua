4396aad553e8eb27aa1641010cb1c9b660ae82e3
dreamness <dreamness@163.com>
2025-02-21 19:02:15 +0800

    upload

--- additional/ra3_lisp/demo.lua
+++ additional/ra3_lisp/demo.lua
@@ -18,7 +18,7 @@ function __analyse_token(token)
 end
 
 
-function lexical_analyse(cmd)
+function tokenize(cmd)
     local tokens = {}
 
     local loading_str = ''
@@ -92,68 +92,160 @@ function array_split(arr, start, end_pos)
     return ret
 end
 
+function print_arr(t)
+    -- local s = '['
+    -- for i = 1, getn(t) do
+    --     s = s .. tostring(t[i].token) .. ', '
+    -- end
+    -- s = s .. ']'
+    -- print(s)
+end
 
--- ast: {token, children}
-function grammar_analyse(tokens)
-    local ast = {token=nil, children={}}
+function print_tokens(tokens)
+    local s = 'tokens: ['
+    for i = 1, getn(tokens) do
+        if(tokens[i] == nil) then
+            s = s .. '<nil>, '
+        else
+            s = s .. tostring(tokens[i].token_value) .. ', '
+        end
+    end
+    s = s .. ']'
+    print(s)
+end
+
+function print_ast(ast)
+    if ast.token ~= nil then
+        print(ast.token.token_value)
+    end
+    for i = 1, getn(ast.children) do
+        print_ast(ast.children[i])
+    end
+end
+
+function parse(tokens)
+
     local token_cnt = getn(tokens)
+    print('token_cnt: ' .. tostring(token_cnt))
+    print_tokens(tokens)
+
+
+    local ast = {token=nil, children={}}
     if token_cnt == 0 then
-        return ast
+        print('error: empty token')
+        return nil, 'empty token'
     end
     if token_cnt == 1 then
+        print('------')
         local token = tokens[1]
-        if(token == '(' or token == ')') then
-            return nil, 'invalid token: ' .. tostring(token) 
+        if token.token_type == 'left_bracket' or token.token_type == 'right_bracket' then
+            print('error: invalid token: ' .. tostring(token.token_value))
+            return nil, 'invalid token: ' .. tostring(token.token_value)
         end
         ast.token = token
+        print(ast.token.token_value)
         return ast
     end
-
-    local stack = {}
-    if tokens[1].token_type ~= 'left_bracket' then
+    if token_cnt == 2 then
+        print('error: invalid token: ' .. tostring(tokens[1].token_value))
         return nil, 'invalid token: ' .. tostring(tokens[1].token_value)
     end
-    tinsert(stack, tokens[1])
-    for i = 2, token_cnt do
+    if tokens[1].token_type ~= 'left_bracket' then
+        print('error: invalid token: ' .. tostring(tokens[1].token_value))
+        return nil, 'invalid token: ' .. tostring(tokens[1].token_value)
+    end
+    if tokens[token_cnt].token_type ~= 'right_bracket' then
+        print('error: invalid token: ' .. tostring(tokens[token_cnt].token_value))
+        return nil, 'invalid token: ' .. tostring(tokens[token_cnt].token_value)
+    end
+    ast.token = tokens[2]
+    local stack = {}
+    for i = 3, token_cnt - 1 do
         local token = tokens[i]
         if token.token_type == 'left_bracket' then
             tinsert(stack, token)
         elseif token.token_type == 'right_bracket' then
             if getn(stack) == 0 then
+                print('error: invalid token: ' .. tostring(token.token_value))
                 return nil, 'invalid token: ' .. tostring(token.token_value)
             end
+            tinsert(stack, token)
+            print_arr(stack)
             local left_bracket_index = nil
-            for i = i, 1, -1 do
-                if tokens[i].token_type == 'left_bracket' then
-                    left_bracket_index = i
+            for j = getn(stack) - 1, 1, -1 do
+                if stack[j].token_type == 'left_bracket' then
+                    left_bracket_index = j
                     break
                 end
             end
-            tinsert(ast.children, grammar_analyse(array_split(tokens, left_bracket_index, i)))
+
+            local sub_ast = parse(array_split(stack, left_bracket_index, getn(stack)))
+            -- print('left_bracket_index: ' .. tostring(left_bracket_index))
+            -- print('sub_ast: ')
+            -- print_ast(sub_ast)
+            tinsert(ast.children, sub_ast)
+            for j = getn(stack) - left_bracket_index + 1, 1, -1 do
+                tremove(stack)
+            end
+            print('after remove stack...')
+            print_arr(stack)
         else
-            tinsert(ast.children, token)
+            print('======')
+            if getn(stack) == 0 then
+                print('!!!!!!!')
+                tinsert(ast.children, parse({token}))
+            else
+                tinsert(stack, token)
+            end
         end
+    end
+    return ast
+end
+
+-- env = {}
+function execute(ast)
+    local token = ast.token
+    if token.token_type == 'number' or token.token_type == 'string' or token.token_type == 'nil' then
+        return token.token_value
+    elseif token.token_type == 'symbol' then
+        local token_value = token.token_value
+        local children_cnt = getn(token.children)
+        if token_value == 'define' then
+            if children_cnt < 2 then
+                print('error: bad define')
+                return nil, 'bad define'
+            end
+
+        else
+        end
+    else
+        return nil
     end
 end
 
-function execute(ast)
-end
 
 
 function eval(cmd)
-    local tokens = lexical_analyse(cmd)
+    local tokens = tokenize(cmd)
     print('---- TOKEN -----')
     for i = 1, getn(tokens) do
         print(tokens[i].token_value, tokens[i].token_type)
     end
     
-    local ast = grammar_analyse(tokens)
+    local ast, error = parse(tokens)
+    print('---- AST -----')
+    
+    print('error:' .. tostring(error))
+
+    print_ast(ast)
+
     local ret = execute(ast)
     print('---- RESULT -----')
     print(ret)
 end
 
 
-
-
-eval("(add a nil)")
+-- eval('(define x 1) (define y 2) (+ x y)')
+-- eval('(define (x a b) (+ a b))')
+-- eval('(+ 1 2)')
+eval('nil')
--- additional/ra3_lisp/demo2.lua
+++ additional/ra3_lisp/demo2.lua
@@ -1,146 +1,100 @@
--- Lisp 解释器实现
-function new_env(parent)
-    local env = { parent = parent, vars = {} }
-    env["nil"] = nil
-    return env
-end
 
-global_env = new_env(_G) -- 继承 Lua 全局环境
+-- LISP解释器核心组件
+local env = {
+    ["+"] = function(args) return args + args end,
+    ["-"] = function(args) return args - args end,
+    ["*"] = function(args) return args * args end,
+    ["/"] = function(args) return args / args end,
+    ["print"] = function(args) print(args) return args end
+}
 
-function lookup_var(env, name)
-    while env do
-        if env.vars[name] ~= nil then
-            return env.vars[name]
-        end
-        env = env.parent
-    end
-    return nil
-end
-
-function eval(expr, env)
-    if type(expr) == "table" then
-        -- 列表求值
-        local first = expr[1]
-        
-        -- 特殊形式处理
-        if first == "def" then
-            local name = expr[2]
-            local value = eval(expr[3], env)
-            env.vars[name] = value
-            return value
-        elseif first == "fn" then
-            local params = expr[2]
-            local body = expr[3]
-            return function(...)
-                local args = arg
-                local new_env = new_env(env)
-                for i=1,getn(params) do
-                    new_env.vars[params[i]] = args[i]
-                end
-                return eval(body, new_env)
-            end
-        elseif first == "if" then
-            local cond = eval(expr[2], env)
-            if cond then
-                return eval(expr[3], env)
-            else
-                return eval(expr[4], env)
-            end
-        else
-            -- 函数调用
-            local f = eval(first, env)
-            local args = {}
-            for i=2,getn(expr) do
-                tinsert(args, eval(expr[i], env))
-            end
-            return f(unpack(args))
-        end
-    elseif type(expr) == "string" then
-        -- 变量查找
-        local val = lookup_var(env, expr)
-        if val == nil then
-            error("Undefined variable: "..expr)
-        end
-        return val
-    else
-        -- 字面量
-        return expr
-    end
-end
-
--- Lisp 解析器
-function parse(str)
+function tokenize(s)
     local tokens = {}
-    str = gsub(str, "(%()", " %1 ")
-    str = gsub(str, "(%))", " %1 ")
-    str = gsub(str, "\"", " \" ")
-    
-    for token in strgfind(str, "[^ \t\n\r]+") do
-        if token == "(" then
-            tinsert(tokens, { tag = "list_start" })
-        elseif token == ")" then
-            tinsert(tokens, { tag = "list_end" })
+    local i = 1
+    while i <= string.len(s) do
+        local c = string.sub(s, i, i)
+        if c == '(' or c == ')' then
+            tinsert(tokens, c)
+            i = i + 1
+        elseif c == ' ' or c == '\t' or c == '\n' then
+            i = i + 1
         else
-            -- 识别数据类型
-            local num = tonumber(token)
-            if token == "nil" then
-                tinsert(tokens, nil)
-            elseif num then
-                tinsert(tokens, num)
-            elseif strsub(token, 1, 1) == "\"" then
-                tinsert(tokens, gsub(token, "\"", ""))
-            else
-                tinsert(tokens, token)
+            local start = i
+            while i <= string.len(s) do
+                local c = string.sub(s, i, i)
+                if c == '(' or c == ')' or c == ' ' or c == '\t' or c == '\n' then
+                    break
+                end
+                i = i + 1
+            end
+            tinsert(tokens, string.sub(s, start, i-1))
+        end
+    end
+    return tokens
+end
+
+function parse(tokens)
+    local pos = 1
+    local function walk()
+        local token = tokens[pos]
+        pos = pos + 1
+        
+        if token == '(' then
+            local node = {}
+            while tokens[pos] ~= ')' do
+                table.insert(node, walk())
+            end
+            pos = pos + 1
+            return node
+        elseif token == ')' then
+            error("unexpected )")
+        else
+            return tonumber(token) or token
+        end
+    end
+    return walk()
+end
+
+function eval(exp, env)
+    if type(exp) == "number" then
+        return exp
+    elseif type(exp) == "string" then
+        return env[exp]
+    elseif type(exp) == "table" then
+        if exp == "define" then
+            local _, name, value = unpack(exp)
+            env[name] = eval(value, env)
+            return env[name]
+        elseif exp == "lambda" then
+            return {
+                type = "lambda",
+                params = exp,
+                body = exp,
+                env = env
+            }
+        else
+            local fn = eval(exp, env)
+            local args = {}
+            for i=2, #exp do
+                table.insert(args, eval(exp[i], env))
+            end
+            
+            if type(fn) == "function" then
+                return fn(args)
+            elseif fn.type == "lambda" then
+                local new_env = {}
+                for k,v in pairs(fn.env) do new_env[k] = v end
+                for i,param in ipairs(fn.params) do
+                    new_env[param] = args[i]
+                end
+                return eval(fn.body, new_env)
             end
         end
     end
-    
-    local stack = {}
-    local current = {}
-    tinsert(stack, current)
-    
-    for _,token in ipairs(tokens) do
-        if type(token) == "table" then
-            if token.tag == "list_start" then
-                local new_list = {}
-                tinsert(current, new_list)
-                tinsert(stack, current)
-                current = new_list
-            else
-                current = tremove(stack)
-            end
-        else
-            tinsert(current, token)
-        end
-    end
-    
-    return current[1]
 end
 
--- REPL 交互
-function repl(input)
-
-        if input == "exit" then break end
-        
-        local ok, result = pcall(function()
-            local parsed = parse(input)
-            return eval(parsed, global_env)
-        end)
-        
-        if ok then
-            if result == nil then
-                print("nil")
-            else
-                print(result)
-            end
-        else
-            print("Error: "..result)
-        end
-end
-
--- 测试示例
-print("Simple Lisp interpreter (Lua 4)")
-print("Try: (def add (fn (a b) (+ a b)))")
-print("Then: (add 3 4)")
-repl("(def add (fn (a b) (+ a b)))")
-repl("(add 3 4)")
+-- 使用示例
+local code = "(begin (define a 1) (define b (+ a 2)) (print b))"
+local tokens = tokenize(code)
+local ast = parse(tokens)
+eval(ast, env)  -- 输出3
